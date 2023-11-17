fdab924202c6abd13d4c2d186597cbdc54a70fba
MD FIROJ AHMED <74416772+firojahmed1313@users.noreply.github.com>
2023-11-17 21:10:05 +0530

    Add files via upload

---
+++ routers/userRouts.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { userLogin, userLogout, userRegister,getMyProfile } from "../Controlers/userControler.js";
+import { isSignin } from "../middlewares/auth.js";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("<h1>MD FIROJ AHMED</h1>");
+});
+
+router.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+router.post("/api/user/register", userRegister);
+
+router.get("/logIn", (req, res) => {
+  res.render("logIn.ejs");
+});
+
+router.post("/api/user/logIn", userLogin);
+
+router.get("/api/user/logOut", userLogout);
+
+router.get("/user/myProfile",isSignin, getMyProfile);
+
+
+export default router;
