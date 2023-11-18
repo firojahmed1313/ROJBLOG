74d4e4f517f7858c2e90d765bc3d5d616a50fa40
MD FIROJ AHMED <74416772+firojahmed1313@users.noreply.github.com>
2023-11-18 20:13:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 //app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://5173-mdfirojahmed30-rojblock-hg4t4aboodn.ws-us106.gitpod.io");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
