afbfc159c68793bcd2f50e3a03108202bed7ef4d
MD FIROJ AHMED <74416772+firojahmed1313@users.noreply.github.com>
2023-11-29 13:09:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import blogRouters from "./routers/blogRouts.js"
 import cookieParser from "cookie-parser";
 import cors from "cors"
 const app = express();
-//app.use(express.json());
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
