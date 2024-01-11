8845e189a9440ace3dd5f793bfb4da5a91e954fc
MD FIROJ AHMED <74416772+firojahmed1313@users.noreply.github.com>
2024-01-11 20:17:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cookieParser())
 app.use(cors({
   origin: process.env.FRONTED_URL,
   methods:["GET" ,"POST" , "PUT" , "DELETE"],
-  credentials:true,
+  credentials: true,
   optionSuccessStatus:200
 }))
 
